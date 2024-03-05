6e1f007e16d65e7c60825f09e89b38e760bbe374
Adam Ługowski <adam.lugowski@live.com>
2024-03-05 12:41:09 +0100

    made foreign key in database, user input, connected tasks and main.py

--- database.py
+++ database.py
@@ -4,14 +4,25 @@ import sqlite3
 def init_db():
     with sqlite3.connect('todo') as db:
         cursor = db.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS tasks(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            category INTEGER
-        )''')
-    with sqlite3.connect('todo') as db:
-        cursor = db.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS categories(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id INTEGER,
             category_name TEXT
         )''')
+        db.commit()
+    with sqlite3.connect('todo') as db:
+        categories_data = [
+            (1, 'event'),
+            (2, 'todo')
+        ]
+        cursor = db.cursor()
+        cursor.executemany('INSERT INTO categories (id, category_name) VALUES (?, ?)', categories_data)
+        db.commit()
+    with sqlite3.connect('todo') as db:
+        cursor = db.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS tasks(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category_id INTEGER,
+            name TEXT,
+            FOREIGN KEY(category_id) REFERENCES categories(id)
+        )''')
+        db.commit()
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from database import init_db
+import sqlite3
+from tasks import todo_manager
 
 
 if __name__ == '__main__':
     init_db()
-
+    # in this section I will ask user how many tasks to add
+    for _ in range(2):
+        # in this section I will handle exceptions
+        category = input('Category name: ')
+        name = input('Task name: ')
+        user_task = todo_manager.make_task(category, name)
+        with sqlite3.connect('todo') as db:
+            cursor = db.cursor()
+            cursor.execute('INSERT INTO tasks (category_id, name) VALUES (?, ?)',
+                           (user_task.task_type, user_task.description))
