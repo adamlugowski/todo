46fc051eda222021a68ab96dc2cc538ae11f92ac
Adam Ługowski <adam.lugowski@live.com>
2024-03-08 13:56:30 +0100

    enable user choice of number of tasks

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ from tasks import todo_manager
 
 if __name__ == '__main__':
     init_db()
-    for _ in range(2):
+    print('This app provide you adding up to 5 tasks')
+    while True:
+        number_of_tasks = int(input('How many tasks would you like to add? '))
+        try:
+            if number_of_tasks not in range(5):
+                raise Exception('You should type 1, 2, 3, 4, 5')
+        except Exception as message:
+            print(message)
+        if number_of_tasks in range(5):
+            break
+    for _ in range(number_of_tasks):
         while True:
             category = input('Category name: ')
             try:
@@ -26,7 +36,6 @@ if __name__ == '__main__':
 
         with sqlite3.connect('todo') as db:
             cursor = db.cursor()
-            cursor.execute('INSERT INTO tasks (category_id, name) VALUES (?, ?)',(user_task.task_type,
-                                                                                  user_task.description))
+            cursor.execute('INSERT INTO tasks (category_id, name) VALUES (?, ?)',(user_task.task_type, user_task.description))
             db.commit()
 
